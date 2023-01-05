acbe712c01cd81ac9e942a0513b6bb0ad53397ba
Danniel Navas <dannielnavas@gmail.com>
2023-01-05 06:09:38 -0500

    ✅ update test

--- src/app/auth/components/register-form/register-form.component.spec.ts
+++ src/app/auth/components/register-form/register-form.component.spec.ts
@@ -1,18 +1,25 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { RegisterFormComponent } from './register-form.component';
 
 describe('RegisterFormComponent', () => {
   let component: RegisterFormComponent;
   let fixture: ComponentFixture<RegisterFormComponent>;
+  let httpController: HttpTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RegisterFormComponent ]
-    })
-    .compileComponents();
+      declarations: [RegisterFormComponent],
+      imports: [ReactiveFormsModule, HttpClientTestingModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(RegisterFormComponent);
+    httpController = TestBed.inject(HttpTestingController);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/service/user.service.spec.ts
+++ src/app/service/user.service.spec.ts
@@ -1,13 +1,21 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { UsersService } from './user.service';
 
 describe('UserService', () => {
   let service: UsersService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(UsersService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
